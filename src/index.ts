6a72bba48ce99a661c42d72dc7660c5617cbada1
Kim Song <kimsong432@gmail.com>
2022-05-03 10:25:45 +0700

    spliting for services file

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+export const { DOCUMENT_ID, TOKEN, SERVER_URL, CONNECTION_STRING } =
+  process.env;
+export const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const URI = `/webhook/${TOKEN}`;
+const WEBHOOK_URL = SERVER_URL + URI;
+const port = process.env.PORT || 5000;
+
+const app: Express = express();
+app.use(bodyParser.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
---
+++ src/services.ts
@@ -0,0 +1,100 @@
+import axios from "axios";
+import { MongoClient, ObjectId, WithId } from "mongodb";
+import { CONNECTION_STRING, DOCUMENT_ID, TELEGRAM_API } from "./index";
+
+export const dbClient = new MongoClient(`${CONNECTION_STRING}`);
+
+type DBSchema = {
+  _id: ObjectId;
+  chat_id: number;
+  admins: number[];
+  day_count: number;
+};
+
+export type Cache = DBSchema | undefined;
+
+export let cache: Cache;
+
+export const fetchAndCache = async () => {
+  try {
+    await dbClient.connect();
+    if (DOCUMENT_ID) {
+      const document = await findOneDocument(DOCUMENT_ID);
+      if (document) {
+        //@ts-ignore
+        cache = document;
+      }
+    }
+  } catch (err) {
+    throw new Error(`function: "fetchAndCache"\nError:\n${err}`);
+  } finally {
+    await dbClient.close();
+  }
+};
+
+export const findOneDocument = async (documentId: string) => {
+  try {
+    if (documentId) {
+      const document = await dbClient
+        .db("day-count-db")
+        .collection("data")
+        .findOne({ _id: new ObjectId(documentId) });
+      return document;
+    }
+  } catch (err) {
+    throw new Error(
+      `function: "findOneDocument"\nDOCUMENT_ID: ${documentId}\nError:\n${err},`
+    );
+  }
+};
+
+export const increaseDayCount = async () => {
+  try {
+    await dbClient.connect();
+    const collection = await dbClient
+      .db("day-count-db")
+      .collection("data")
+      .findOneAndUpdate(
+        { _id: new ObjectId(DOCUMENT_ID) },
+        { $inc: { day_count: 1 } }
+      );
+    if (collection.ok) {
+      //@ts-ignore
+      cache = collection.value;
+      return collection.value;
+    } else {
+      return null;
+    }
+  } catch (err) {
+    throw new Error(`function: "increaseDayCount"\nError:\n${err}`);
+  } finally {
+    await dbClient.close();
+  }
+};
+
+export const sendMessage = async (chat_id: number, message: string) => {
+  if (!chat_id || !message) return;
+  try {
+    await axios.post(`${TELEGRAM_API}/sendMessage`, {
+      chat_id,
+      text: message,
+    });
+  } catch (err) {
+    throw new Error(
+      `function: "sendMessage"\nchat_id: ${chat_id}\nmessage: ${message}\n${err}`
+    );
+  }
+};
+
+export const sendMessageToGroup = async (message: string) => {
+  if (!message) return;
+
+  try {
+    if (!cache?.chat_id) await fetchAndCache();
+    cache?.chat_id && (await sendMessage(cache?.chat_id, message));
+  } catch (err) {
+    throw new Error(
+      `function: "sendMessageToGroup"\nmessage: ${message}\nError:\n${err}`
+    );
+  }
+};
